8143d5d8a9adf31a80a3c8ce0a57ca0aab212bc8
Pelangi Masita <pelangimasita18@gmail.com>
2024-09-08 18:58:28 +0700

    rotating

--- tugas2/app.js
+++ tugas2/app.js
@@ -5,11 +5,14 @@ var vertexShaderText =
 'attribute vec2 vertPosition;',
 'attribute vec3 vertColor;',
 'varying vec3 fragColor;',
+'uniform mat4 mWorld;',
+'uniform mat4 mView;',
+'uniform mat4 mProj;',
 '',
 'void main()',
 '{',
 '  fragColor = vertColor;',
-'  gl_Position = vec4(vertPosition, 0.0, 1.0);',
+'  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 0.0, 1.0);',
 '}'
 ].join('\n');
 
