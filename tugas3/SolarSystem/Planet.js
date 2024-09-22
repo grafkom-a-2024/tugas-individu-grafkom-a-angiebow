aa698ea1c671397e983c80e68192e6df6d82c5e8
Pelangi Masita <pelangimasita18@gmail.com>
2024-09-22 16:24:25 +0700

    SolarSystem

--- tugas3/SolarSystem/Planet.js
+++ tugas3/SolarSystem/Planet.js
@@ -1,9 +1,3 @@
-// Solar System in Processing - Part 2 (3D)
-// The Coding Train / Daniel Shiffman
-// https://thecodingtrain.com/CodingChallenges/008-solarsystemgenerator3d.html
-// https://youtu.be/dncudkelNxw
-// https://editor.p5js.org/codingtrain/sketches/QIB-sT10
-
 class Planet {
   constructor(r, d, o) {
     this.v = p5.Vector.random3D();
@@ -45,19 +39,15 @@ class Planet {
     noStroke();
     let v2 = createVector(1, 0, 1);
     let p = this.v.cross(v2);
-    // Rotation around a 0-length axis doesn't work in p5.js, so don't do that.
     if (p.x != 0 || p.y != 0 || p.z != 0) {
       rotate(this.angle, p);
     }
     stroke(255);
-    //line(0, 0, 0, this.v.x, this.v.y, this.v.z);
-    //line(0, 0, 0, p.x, p.y, p.z);
 
     translate(this.v.x, this.v.y, this.v.z);
     noStroke();
     fill(255);
     sphere(this.radius);
-    //ellipse(0, 0, this.radius * 2, this.radius * 2);
     if (this.planets != null) {
       for (let i = 0; i < this.planets.length; i++) {
         this.planets[i].show();
